7dfaa13f75e15173ae6e799c0b3cd47237f5335f
pingxixiaohua <1608386461@qq.com>
2020-09-22 20:04:20 +0800

    修改模板文件的网址和联系人

--- controllers/default.go
+++ controllers/default.go
@@ -9,7 +9,7 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["Website"] = "www.baidu.com"
+	c.Data["Email"] = "1608386461@qq.com"
 	c.TplName = "index.tpl"
 }
