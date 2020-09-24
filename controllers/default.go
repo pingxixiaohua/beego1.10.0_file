c468be11edc4486f84d1ff433c6b750fa76fd9e9
pingxixiaohua <1608386461@qq.com>
2020-09-24 20:29:56 +0800

    get请求的数据解析，post请求的数据解析

--- controllers/default.go
+++ controllers/default.go
@@ -1,7 +1,11 @@
 package controllers
 
 import (
+	"beegofile/models"
+	"encoding/json"
+	"fmt"
 	"github.com/astaxie/beego"
+	"io/ioutil"
 )
 
 type MainController struct {
@@ -9,7 +13,61 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
+	userName := c.Ctx.Input.Query("user")
+	password := c.Ctx.Input.Query("psd")
+	//使用固定数据进行数据校验
+	if userName != "admin" || password != "123456"{
+		c.Ctx.ResponseWriter.Write([]byte("对不起，数据校验错误"))
+		return
+	}
+
+	c.Ctx.ResponseWriter.Write([]byte("恭喜，数据校验成功"))
+
 	c.Data["Website"] = "www.baidu.com"
 	c.Data["Email"] = "1608386461@qq.com"
 	c.TplName = "index.tpl"
+}
+
+
+
+//func (c * MainController) Post(){
+////	//1.接受解析post请求的参数
+////	name := c.Ctx.Request.FormValue("name")
+////	age := c.Ctx.Request.FormValue("age")
+////	sex := c.Ctx.Request.FormValue("sex")
+////	fmt.Println(name,age,sex)
+////	//数据校验
+////	if name != "admin" && age != "15"{
+////		//c.Ctx.ResponseWriter.Write([]byte(""))
+////		c.Ctx.WriteString("数据校验失败")
+////		return
+////	}
+////	c.Ctx.WriteString("数据校验成功")
+////}
+
+
+//该方法用于处理post类型的请求
+
+func (c * MainController) Post(){
+	//解析前端提交的json格式的数据
+	var person models.Person
+	dataBytes, err := ioutil.ReadAll(c.Ctx.Request.Body)
+	if err != nil {
+		c.Ctx.WriteString("数据解析错误")
+		return
+	}
+	err = json.Unmarshal(dataBytes,&person)
+	if err != nil {
+		c.Ctx.WriteString("数据解析错误，请重试")
+		return
+	}
+	fmt.Println("姓名:",person.Name)
+	fmt.Println("年龄:",person.Age)
+	fmt.Println("性别:",person.Sex)
+	c.Ctx.WriteString("数据解析成功")
+}
+
+
+func (c * MainController) Delete(){
+
 }
---
+++ models/person.go
@@ -0,0 +1,8 @@
+package models
+
+//人结构体的定义，包含三个字段：name,age,sex,与前端一致
+type Person struct {
+	Name string
+	Age int
+	Sex string
+}
